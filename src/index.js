019e1ae14e2a24d9d02f5083689e16e4071570b9
Mainulhasan05 <mdrifatbd5@gmail.com>
2024-09-02 13:24:00 +0600

    updated

--- src/index.js
+++ src/index.js
@@ -18,6 +18,11 @@ app.use(bodyParser.json());
 // Database Connection
 connectDB();
 
+app.get('/', (req, res) => {
+    // welcome and return the ip address of the client
+    res.send(`Welcome to the Water Tracker API. Your IP address is ${req.ip}`);
+    });
+
 // Routes
 app.use('/api', authRoutes);
 app.use('/api', userRoutes);
